BenPope/redpanda

metrics: Allow configuration of default aggregations
Signed-off-by: Ben Pope <ben@redpanda.com>

src/v/config/configuration.cc
@@ -237,6 +237,12 @@ configuration::configuration()
       "Disable registering metrics",
       base_property::metadata{},
       false)
+  , aggregate_metrics(
+      *this,
+      "aggregate_metrics",
+      "Enable default aggregatations for metrics",
+      {.needs_restart = needs_restart::yes},
+      true)
   , group_min_session_timeout_ms(
       *this,
       "group_min_session_timeout_ms",

VladLazar/seastar

src/v/config/configuration.cc
@@ -237,6 +237,12 @@ configuration::configuration()
       "Disable registering metrics",
       base_property::metadata{},
       false)
+  , aggregate_metrics(
+      *this,
+      "aggregate_metrics",
+      "Enable default aggregations for metrics",
+      {.needs_restart = needs_restart::yes},
+      true)
   , group_min_session_timeout_ms(
       *this,
       "group_min_session_timeout_ms",